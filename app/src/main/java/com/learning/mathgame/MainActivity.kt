82cd9c3ce0e7649cd9f79b47a7f9b78c116d6af1
zahramarri <zahramarri.dev@gmail.com>
2022-07-31 12:24:32 +0430

    set viewBinding

--- app/src/main/java/com/learning/mathgame/MainActivity.kt
+++ app/src/main/java/com/learning/mathgame/MainActivity.kt
@@ -2,10 +2,13 @@ package com.learning.mathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.learning.mathgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
