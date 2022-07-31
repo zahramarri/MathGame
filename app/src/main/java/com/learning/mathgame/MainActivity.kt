e02bc8159c4b6d65935973cc9bbef91ae4aa3299
zahramarri <zahramarri.dev@gmail.com>
2022-07-31 17:42:30 +0430

    declare fun setPrimaryViewsState

--- app/src/main/java/com/learning/mathgame/MainActivity.kt
+++ app/src/main/java/com/learning/mathgame/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.learning.mathgame
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.learning.mathgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +10,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            setPrimaryViewsState()
+        }
+    }
+
+    private fun setPrimaryViewsState() {
+        binding.btnStart.visibility = View.GONE
+
+        binding.tvDiceA.visibility = View.VISIBLE
+        binding.tvDiceB.visibility = View.VISIBLE
+        binding.tvMathOperator.visibility = View.VISIBLE
+        binding.tvQuestionMark.visibility = View.VISIBLE
     }
 }
